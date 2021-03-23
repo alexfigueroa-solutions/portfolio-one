caaff3991d64ce782a931674a19618e0e97c3d7e
Alejandro Figueroa <cybrvybe@gmail.com>
2021-03-23 14:24:06 -0700

    Added CapabiltiesArray and CapabilityHeaderArray to capabilities_section.js. This is because the component, CapabilitySection, depends on the data from CapabilitiesArray and CapabiltiesHeaderArray. Still need to finish CapabilitiesArray and create CapabilitySection component.

---
+++ gatsby-starter-default/src/components/capabilities_section/capabilities_section.js
@@ -0,0 +1,34 @@
+
+const CapabilityHeadersArray = [
+    'Back-End',
+    'Database',
+    'Data Science',
+    'Automation',
+    'DevOps',
+    'Front-End'
+]
+
+
+const CapabilitiesArray = [
+    {
+        title: 'UI Design',
+        header: CapabilityHeadersArray[5],
+        description: 'Employs a holistic approach by using a component-based UI design process.'
+    },
+    {
+        title: 'UI Development',
+        header: CapabilityHeadersArray[5],
+        description: 'Creates smooth, responsive, and dynamic UIs using various modern web technologies and frameworks.'
+    },
+    {
+        title: 'ORM Data Modeling',
+        header: CapabilityHeadersArray[1],
+        description: 'Implements Object Relational Mapping (ORM) to model business and personal data effectively.'
+    },
+    {
+        title: 'Graph Database',
+        header: CapabilityHeadersArray[1],
+        description: 'Implements graph databases to enhance general understanding of business data, as well as to safely, and efficiently, accumulate data - a massively proliferating, valuable asset in the 21st Century.'
+    }
+
+]
