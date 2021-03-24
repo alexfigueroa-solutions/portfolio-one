f0d8c9504ad924f3751f90c15b6645febcf2fcf9
Alejandro Figueroa <cybrvybe@gmail.com>
2021-03-24 15:20:36 -0700

    Created TabLayout React component. This is in order to use in CapabilitySection component. Still need to implement and test.

---
+++ gatsby-starter-default/src/components/tab_layout/tab_layout.js
@@ -0,0 +1,35 @@
+import React from 'React';
+
+class TabLayout extends React.Component{
+    render(){
+        const{
+            nav_header_array,
+            content_array
+        }
+        <div class = 'container'>
+            <div class = 'nav-container'>
+                <ul class = 'nav-list'>
+                    {nav_header_array.map(
+                        header =>(
+                            <li class = 'nav-btn'>{header}</li>
+                        )
+                    )
+                    }
+                </ul>
+            </div>
+            <div class = 'content-container'>
+                {content_array.map(
+                    content => (
+                        <div class = "subcontainer">
+                            <h1>{content.title}</h1>
+                            <h2>{content.description}</h2>
+                        </div>
+                    )
+                )}
+            </div>
+            
+
+        </div>
+    }
+}
+export default TabLayout
